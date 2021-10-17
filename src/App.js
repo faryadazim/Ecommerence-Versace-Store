6c8420eaf69c4ac70417b146efe3e246ebe161bc
Faryad Az Zim <73974353+faryadazim@users.noreply.github.com>
2021-10-16 19:39:19 -0700

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import "./App.css";
+import { FaBeer } from "react-icons/fa";
 import { AiOutlineShoppingCart } from "react-icons/ai";
 
 function App() {
   return (
     <div>
-      Ecooomerence <AiOutlineShoppingCart />
+      <FaBeer />
+      npm install --global yarn <AiOutlineShoppingCart />
     </div>
   );
 }
