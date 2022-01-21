3dd2a7d48b29c6dfeaff09b96a405ecaf9afc375
Faryad Az Zim <73974353+faryadazim@users.noreply.github.com>
2022-01-22 02:39:38 +0500

    Hero Section added

--- src/App.js
+++ src/App.js
@@ -2,6 +2,10 @@ import "./App.css";
 import Nav from "./components/Nav/Nav";
 import HeroSection from "./components/Others/HeroSection";
 import GiftFinder from "./components/Others/GiftFinder";
+
+import product_1 from './assets/collections/Shop/product_1.webp'
+
+
 function App() {
   return (
     <div className="pb-5 mb-5">
@@ -13,27 +17,24 @@ function App() {
         <div className="container-fluid px-4">
           <div className="row px-2">
             <div className="col-md-3  ">
-              <div className="mx-2 bg-warning">
-
-                Lorem ipsum dolor sit amet consectetur adipisicing elit. Cumque aliquid, saepe commodi culpa tempore, atque enim eaque dolor laudantium illum fuga sit reprehenderit eligendi autem assumenda officia hic excepturi dignissimos.
+              <div className="mx-2 bg-warning">  <div className="px-2">
+                <img src={product_1} alt="product_1" /></div>
               </div>
             </div>
             <div className="col-md-3  ">
-              <div className="mx-2 bg-danger">
-
-                Lorem ipsum dolor sit amet consectetur adipisicing elit. Cumque aliquid, saepe commodi culpa tempore, atque enim eaque dolor laudantium illum fuga sit reprehenderit eligendi autem assumenda officia hic excepturi dignissimos.
+              <div className="mx-2 bg-danger">  <div className="px-2">
+                <img src={product_1} alt="product_1" /></div>
               </div>
             </div>
             <div className="col-md-3  ">
-              <div className="mx-2 bg-secondary text-light">
-
-                Lorem ipsum dolor sit amet consectetur adipisicing elit. Cumque aliquid, saepe commodi culpa tempore, atque enim eaque dolor laudantium illum fuga sit reprehenderit eligendi autem assumenda officia hic excepturi dignissimos.
+              <div className="mx-2 bg-secondary text-light">  <div className="px-2">
+                <img src={product_1} alt="product_1" /></div>
               </div>
             </div>
             <div className="col-md-3  ">
               <div className="mx-2 bg-primary">
-
-                Lorem ipsum dolor sit amet consectetur adipisicing elit. Cumque aliquid, saepe commodi culpa tempore, atque enim eaque dolor laudantium illum fuga sit reprehenderit eligendi autem assumenda officia hic excepturi dignissimos.
+                <div className="px-2">
+                  <img src={product_1} alt="product_1" /></div>
               </div>
             </div>
           </div>
